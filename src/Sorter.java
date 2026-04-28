1aa5eacb023739ea70a0ea29e15353dcb3b2c68e
Kalidolda Damilya <dkalidolda@gmail.com>
2026-04-28 17:59:53 +0500

    implemented basic+advanced sorting algorithm

---
+++ src/Sorter.java
@@ -0,0 +1,79 @@
+import java.util.Random;
+
+public class Sorter {
+    public void basicSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j]     = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+
+    public void advancedSort(int[] arr) {
+        if (arr.length < 2) return;
+        mergeSort(arr, 0, arr.length - 1);
+    }
+
+    private void mergeSort(int[] arr, int left, int right) {
+        if (left >= right) return;
+
+        int mid = left + (right - left) / 2;
+
+        mergeSort(arr, left, mid);
+
+        mergeSort(arr, mid + 1, right);
+
+        merge(arr, left, mid, right);
+    }
+
+    private void merge(int[] arr, int left, int mid, int right) {
+        int leftSize  = mid - left + 1;
+        int rightSize = right - mid;
+
+        int[] leftArr  = new int[leftSize];
+        int[] rightArr = new int[rightSize];
+
+        for (int i = 0; i < leftSize; i++)
+            leftArr[i] = arr[left + i];
+        for (int j = 0; j < rightSize; j++)
+            rightArr[j] = arr[mid + 1 + j];
+
+        int i = 0, j = 0, k = left;
+
+        while (i < leftSize && j < rightSize) {
+            if (leftArr[i] <= rightArr[j]) {
+                arr[k++] = leftArr[i++];
+            } else {
+                arr[k++] = rightArr[j++];
+            }
+        }
+
+        while (i < leftSize)  arr[k++] = leftArr[i++];
+
+        while (j < rightSize) arr[k++] = rightArr[j++];
+    }
+
+    public void printArray(int[] arr) {
+        System.out.print("[");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+            if (i < arr.length - 1) System.out.print(", ");
+        }
+        System.out.println("]");
+    }
+
+    public int[] generateRandomArray(int size) {
+        Random rand = new Random();
+        int[] arr   = new int[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = rand.nextInt(10000);
+        }
+        return arr;
+    }
+}
